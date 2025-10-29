ccf73ce80ce892bdcfa3508a344a594c6460b1ad
Barni Seetharaman <barni@google.com>
2025-10-29 09:19:14 +0000

    fix for issue # 117

--- extensions/controllers/sandboxclaim_controller_test.go
+++ extensions/controllers/sandboxclaim_controller_test.go
@@ -42,7 +42,7 @@ func TestSandboxClaimReconcile(t *testing.T) {
 			Namespace: "default",
 		},
 		Spec: extensionsv1alpha1.SandboxTemplateSpec{
-			PodTemplate: corev1.PodTemplateSpec{
+			PodTemplate: v1alpha1.PodTemplate{
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
@@ -67,139 +67,443 @@ func TestSandboxClaimReconcile(t *testing.T) {
 		},
 	}
 
-	uncontrolledSandbox := &v1alpha1.Sandbox{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-claim",
-			Namespace: "default",
-		},
-		Spec: v1alpha1.SandboxSpec{
-			PodTemplate: v1alpha1.PodTemplate{
-				Spec: template.Spec.PodTemplate.Spec,
-			},
-		},
-	}
+		uncontrolledSandbox := &v1alpha1.Sandbox{
 
-	controlledSandbox := &v1alpha1.Sandbox{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-claim",
-			Namespace: "default",
-			OwnerReferences: []metav1.OwnerReference{
-				{
-					APIVersion: "extensions.agents.x-k8s.io/v1alpha1",
-					Kind:       "SandboxClaim",
-					Name:       "test-claim",
-					UID:        "claim-uid",
-					Controller: func(b bool) *bool { return &b }(true),
+			ObjectMeta: metav1.ObjectMeta{
+
+				Name:      "test-claim",
+
+				Namespace: "default",
+
+			},
+
+			Spec: v1alpha1.SandboxSpec{
+
+				PodTemplate: v1alpha1.PodTemplate{
+
+					Spec: template.Spec.PodTemplate.Spec,
+
 				},
-			},
-		},
-		Spec: v1alpha1.SandboxSpec{
-			PodTemplate: v1alpha1.PodTemplate{
-				Spec: template.Spec.PodTemplate.Spec,
-			},
-		},
-	}
 
-	readySandbox := controlledSandbox.DeepCopy()
-	readySandbox.Status.Conditions = []metav1.Condition{
-		{
-			Type:   string(sandboxv1alpha1.SandboxConditionReady),
-			Status: metav1.ConditionTrue,
-		},
-	}
+			},
 
-	testCases := []struct {
-		name              string
-		existingObjects   []client.Object
-		expectSandbox     bool
-		expectError       bool
-		expectedCondition metav1.Condition
-	}{
-		{
-			name:            "sandbox is created when a claim is made",
-			existingObjects: []client.Object{template, claim},
-			expectSandbox:   true,
-			expectedCondition: metav1.Condition{
-				Type:    string(sandboxv1alpha1.SandboxConditionReady),
-				Status:  metav1.ConditionFalse,
-				Reason:  "SandboxNotReady",
-				Message: "Sandbox is not ready",
-			},
-		},
-		{
-			name:            "sandbox is not created when template is not found",
-			existingObjects: []client.Object{claim},
-			expectSandbox:   false,
-			expectError:     true,
-			expectedCondition: metav1.Condition{
-				Type:    string(sandboxv1alpha1.SandboxConditionReady),
-				Status:  metav1.ConditionFalse,
-				Reason:  "TemplateNotFound",
-				Message: `SandboxTemplate "test-template" not found`,
-			},
-		},
-		{
-			name:            "sandbox exists but is not controlled by claim",
-			existingObjects: []client.Object{template, claim, uncontrolledSandbox},
-			expectSandbox:   true,
-			expectError:     true,
-			expectedCondition: metav1.Condition{
-				Type:    string(sandboxv1alpha1.SandboxConditionReady),
-				Status:  metav1.ConditionFalse,
-				Reason:  "ReconcilerError",
-				Message: "Error seen: sandbox \"test-claim\" is not controlled by claim \"test-claim\". Please use a different claim name or delete the sandbox manually",
-			},
-		},
-		{
-			name:            "sandbox exists and is controlled by claim",
-			existingObjects: []client.Object{template, claim, controlledSandbox},
-			expectSandbox:   true,
-			expectedCondition: metav1.Condition{
-				Type:    string(sandboxv1alpha1.SandboxConditionReady),
-				Status:  metav1.ConditionFalse,
-				Reason:  "SandboxNotReady",
-				Message: "Sandbox is not ready",
-			},
-		},
-		{
-			name:            "sandbox exists but template is not found",
-			existingObjects: []client.Object{claim, readySandbox},
-			expectSandbox:   true,
-			expectedCondition: metav1.Condition{
-				Type:    string(sandboxv1alpha1.SandboxConditionReady),
-				Status:  metav1.ConditionTrue,
-				Reason:  "SandboxReady",
-				Message: "Sandbox is ready",
-			},
-		},
-		{
-			name:            "sandbox is ready",
-			existingObjects: []client.Object{template, claim, readySandbox},
-			expectSandbox:   true,
-			expectedCondition: metav1.Condition{
-				Type:    string(sandboxv1alpha1.SandboxConditionReady),
-				Status:  metav1.ConditionTrue,
-				Reason:  "SandboxReady",
-				Message: "Sandbox is ready",
-			},
-		},
-	}
+		}
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			scheme := newScheme(t)
-			client := fake.NewClientBuilder().WithScheme(scheme).WithObjects(tc.existingObjects...).WithStatusSubresource(claim).Build()
-			reconciler := &SandboxClaimReconciler{
-				Client: client,
-				Scheme: scheme,
-			}
-			req := reconcile.Request{
-				NamespacedName: types.NamespacedName{
-					Name:      "test-claim",
-					Namespace: "default",
+	
+
+		controlledSandbox := &v1alpha1.Sandbox{
+
+			ObjectMeta: metav1.ObjectMeta{
+
+				Name:      "test-claim",
+
+				Namespace: "default",
+
+				OwnerReferences: []metav1.OwnerReference{
+
+					{
+
+						APIVersion: "extensions.agents.x-k-s.io/v1alpha1",
+
+						Kind:       "SandboxClaim",
+
+						Name:       "test-claim",
+
+						UID:        "claim-uid",
+
+						Controller: func(b bool) *bool { return &b }(true),
+
+					},
+
 				},
-			}
-			_, err := reconciler.Reconcile(context.Background(), req)
+
+			},
+
+			Spec: v1alpha1.SandboxSpec{
+
+				PodTemplate: v1alpha1.PodTemplate{
+
+					Spec: template.Spec.PodTemplate.Spec,
+
+				},
+
+			},
+
+		}
+
+	
+
+		readySandbox := controlledSandbox.DeepCopy()
+
+		readySandbox.Status.Conditions = []metav1.Condition{
+
+			{
+
+				Type:   string(sandboxv1alpha1.SandboxConditionReady),
+
+				Status: metav1.ConditionTrue,
+
+			},
+
+		}
+
+	
+
+		testCases := []struct {
+
+			name              string
+
+			existingObjects   []client.Object
+
+			expectSandbox     bool
+
+			expectError       bool
+
+			expectedCondition metav1.Condition
+
+		}{
+
+			{
+
+				name:            "sandbox is created when a claim is made",
+
+				existingObjects: []client.Object{template, claim},
+
+				expectSandbox:   true,
+
+				expectedCondition: metav1.Condition{
+
+					Type:    string(sandboxv1alpha1.SandboxConditionReady),
+
+					Status:  metav1.ConditionFalse,
+
+					Reason:  "SandboxNotReady",
+
+					Message: "Sandbox is not ready",
+
+				},
+
+			},
+
+			{
+
+				name:            "sandbox is not created when template is not found",
+
+				existingObjects: []client.Object{claim},
+
+				expectSandbox:   false,
+
+				expectError:     true,
+
+				expectedCondition: metav1.Condition{
+
+					Type:    string(sandboxv1alpha1.SandboxConditionReady),
+
+					Status:  metav1.ConditionFalse,
+
+					Reason:  "TemplateNotFound",
+
+					Message: `SandboxTemplate "test-template" not found`,
+
+				},
+
+			},
+
+			{
+
+				name:            "sandbox exists but is not controlled by claim",
+
+				existingObjects: []client.Object{template, claim, uncontrolledSandbox},
+
+				expectSandbox:   true,
+
+				expectError:     true,
+
+				expectedCondition: metav1.Condition{
+
+					Type:    string(sandboxv1alpha1.SandboxConditionReady),
+
+					Status:  metav1.ConditionFalse,
+
+					Reason:  "ReconcilerError",
+
+					Message: "Error seen: sandbox \"test-claim\" is not controlled by claim \"test-claim\". Please use a different claim name or delete the sandbox manually",
+
+				},
+
+			},
+
+			{
+
+				name:            "sandbox exists and is controlled by claim",
+
+				existingObjects: []client.Object{template, claim, controlledSandbox},
+
+				expectSandbox:   true,
+
+				expectedCondition: metav1.Condition{
+
+					Type:    string(sandboxv1alpha1.SandboxConditionReady),
+
+					Status:  metav1.ConditionFalse,
+
+					Reason:  "SandboxNotReady",
+
+					Message: "Sandbox is not ready",
+
+				},
+
+			},
+
+			{
+
+				name:            "sandbox exists but template is not found",
+
+				existingObjects: []client.Object{claim, readySandbox},
+
+				expectSandbox:   true,
+
+				expectedCondition: metav1.Condition{
+
+					Type:    string(sandboxv1alpha1.SandboxConditionReady),
+
+					Status:  metav1.ConditionTrue,
+
+					Reason:  "SandboxReady",
+
+					Message: "Sandbox is ready",
+
+				},
+
+			},
+
+			{
+
+				name:            "sandbox is ready",
+
+				existingObjects: []client.Object{template, claim, readySandbox},
+
+				expectSandbox:   true,
+
+				expectedCondition: metav1.Condition{
+
+					Type:    string(sandboxv1alpha1.SandboxConditionReady),
+
+					Status:  metav1.ConditionTrue,
+
+					Reason:  "SandboxReady",
+
+					Message: "Sandbox is ready",
+
+				},
+
+			},
+
+			{
+
+				name: "sandbox is created with metadata when a claim is made",
+
+				existingObjects: []client.Object{
+
+					&extensionsv1alpha1.SandboxTemplate{
+
+						ObjectMeta: metav1.ObjectMeta{
+
+							Name:      "test-template-with-metadata",
+
+							Namespace: "default",
+
+						},
+
+						Spec: extensionsv1alpha1.SandboxTemplateSpec{
+
+							PodTemplate: v1alpha1.PodTemplate{
+
+								ObjectMeta: v1alpha1.PodMetadata{
+
+									Labels: map[string]string{
+
+										"foo": "bar",
+
+									},
+
+									Annotations: map[string]string{
+
+										"baz": "qux",
+
+									},
+
+								},
+
+								Spec: corev1.PodSpec{
+
+									Containers: []corev1.Container{
+
+										{
+
+											Name:  "test-container",
+
+											Image: "test-image",
+
+										},
+
+									},
+
+								},
+
+							},
+
+						},
+
+					},
+
+					&extensionsv1alpha1.SandboxClaim{
+
+						ObjectMeta: metav1.ObjectMeta{
+
+							Name:      "test-claim-with-metadata",
+
+							Namespace: "default",
+
+							UID:       "claim-uid-with-metadata",
+
+						},
+
+						Spec: extensionsv1alpha1.SandboxClaimSpec{
+
+							TemplateRef: extensionsv1alpha1.SandboxTemplateRef{
+
+								Name: "test-template-with-metadata",
+
+							},
+
+						},
+
+					},
+
+				},
+
+				expectSandbox: true,
+
+				expectedCondition: metav1.Condition{
+
+					Type:    string(sandboxv1alpha1.SandboxConditionReady),
+
+					Status:  metav1.ConditionFalse,
+
+					Reason:  "SandboxNotReady",
+
+					Message: "Sandbox is not ready",
+
+				},
+
+			},
+
+		}
+
+	
+
+		for _, tc := range testCases {
+
+			t.Run(tc.name, func(t *testing.T) {
+
+				scheme := newScheme(t)
+
+				// special case for the metadata test
+
+				var req reconcile.Request
+
+				var template extensionsv1alpha1.SandboxTemplate
+
+				if tc.name == "sandbox is created with metadata when a claim is made" {
+
+					client := fake.NewClientBuilder().WithScheme(scheme).WithObjects(tc.existingObjects...).WithStatusSubresource(&extensionsv1alpha1.SandboxClaim{}).Build()
+
+					reconciler := &SandboxClaimReconciler{
+
+						Client: client,
+
+						Scheme: scheme,
+
+					}
+
+					req = reconcile.Request{
+
+						NamespacedName: types.NamespacedName{
+
+							Name:      "test-claim-with-metadata",
+
+							Namespace: "default",
+
+						},
+
+					}
+
+					if err := client.Get(context.Background(), types.NamespacedName{Name: "test-template-with-metadata", Namespace: "default"}, &template); err != nil {
+
+						t.Fatalf("get template: (%v)", err)
+
+					}
+
+					_, err := reconciler.Reconcile(context.Background(), req)
+
+					if tc.expectError && err == nil {
+
+						t.Fatal("expected an error but got none")
+
+					}
+
+					if !tc.expectError && err != nil {
+
+						t.Fatalf("reconcile: (%v)", err)
+
+					}
+
+					var sandbox v1alpha1.Sandbox
+
+					err = client.Get(context.Background(), req.NamespacedName, &sandbox)
+
+					if tc.expectSandbox && err != nil {
+
+						t.Fatalf("get sandbox: (%v)", err)
+
+					}
+
+					if diff := cmp.Diff(sandbox.Spec.PodTemplate.ObjectMeta, template.Spec.PodTemplate.ObjectMeta); diff != "" {
+
+						t.Errorf("unexpected sandbox metadata:\n%s", diff)
+
+					}
+
+					return
+
+				}
+
+	
+
+				client := fake.NewClientBuilder().WithScheme(scheme).WithObjects(tc.existingObjects...).WithStatusSubresource(claim).Build()
+
+				reconciler := &SandboxClaimReconciler{
+
+					Client: client,
+
+					Scheme: scheme,
+
+				}
+
+				req = reconcile.Request{
+
+					NamespacedName: types.NamespacedName{
+
+						Name:      "test-claim",
+
+						Namespace: "default",
+
+					},
+
+				}
+
+				_, err := reconciler.Reconcile(context.Background(), req)
 			if tc.expectError && err == nil {
 				t.Fatal("expected an error but got none")
 			}
